6a4fc1bcd2ec6af1d37875b0e15b57d0e4ae1154
/Cedric-hub <cedric.boulade.paris@gmail.com>
2021-01-05 15:06:49 +0100

    heroku-deployment v3

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
-<?php include_once("/home.html");
-include_once("/contact.html");
-include_once("/cv.html");
-include_once("/project.html");
+<?php
+include_once("home.html");
+include_once("contact.html");
+include_once("cv.html");
+include_once("project.html");
 ?>
